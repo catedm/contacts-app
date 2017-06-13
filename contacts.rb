c6a3006f19b3c8e8d8388450802a8b93bd8908bc
David Cate <david@element-360.com>
2017-06-13 15:15:01 -0400

    more commits

--- contacts.rb
+++ contacts.rb
@@ -6,7 +6,20 @@ require 'bcrypt'
 require 'yaml'
 
 configure do
-  set :erb, :escape_html => true
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:contacts] ||= []
+  @contacts = session[:contacts]
+end
+
+def parse_contact
+  full_name = params[:first_name] + " " + params[:last_name]
+  email, address, phone = params[:email], params[:address], params[:phone]
+
+  new_contact = { full_name => { "Email" => email, "Phone" => phone, "Address" => address } }
 end
 
 get "/" do
@@ -17,6 +30,31 @@ get '/add' do
   erb :add
 end
 
+get '/:contact/edit' do
+  contact_to_edit = params[:contact]
+  @contact = @contacts.select { |c| c.keys.first == contact_to_edit }.first
+
+  @first_name, @last_name = @contact.keys.first.split
+  @email = @contact[contact_to_edit]["Email"]
+  @phone = @contact[contact_to_edit]["Phone"]
+  @address = @contact[contact_to_edit]["Address"]
+
+  erb :edit
+end
+
+post '/:contact/delete' do
+  @contacts.reject! { |contact| contact.include?(params[:contact]) }
+  redirect "/"
+end
+
 post "/add_contact" do
+  new_contact = parse_contact
+  session[:contacts] << new_contact
+  redirect "/"
+end
+
+post "/edit_contact" do
+  new_contact = parse_contact
+  session[:contacts] << new_contact
   redirect "/"
 end
